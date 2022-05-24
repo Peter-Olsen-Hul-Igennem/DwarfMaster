88d7d2f5edee9371f15d4f71e9cb2b520e42da1c
Peter Olsen <peter@hul-igennem.dk>
2022-05-24 22:09:54 +0200

    Changed font on start and wait screen

--- src/MidiTransmitter.h
+++ src/MidiTransmitter.h
@@ -1,8 +1,8 @@
 #ifndef DWARFMASTER_MIDI_TRANSMITTER_H
 #define DWARFMASTER_MIDI_TRANSMITTER_H
 
-#include <Arduino.h>
 #include "MidiCtrlData.h"
+#include <Arduino.h>
 
 class MidiTransmitter
 {
@@ -12,9 +12,8 @@ class MidiTransmitter
     void sendExpressionMessages(const uint8_t expPin, const Bank* bank, const bool* toggleStates, const uint8_t currentSelectedBtn);
 
  private:
-    uint8_t prevExpValue = 128;
+    uint8_t prevExpValue  = 128;
     uint64_t prevReadTime = 0;
-    
 };
 
 #endif
